953f5f067c315c6b9d2c184df3417488567d6a8c
alirashidAR <110668489+alirashidAR@users.noreply.github.com>
2023-07-29 23:04:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import './index.css';
+import backgroundwebsite from './images/backgroundwebsite.png'
+import images from './images.js'
 
-function App() {
+
+const ImageSlider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const prevSlide = () => {
+    setSlideIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  const nextSlide = () => {
+    setSlideIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center h-screen"style={{'backgroundImage':`url(${backgroundwebsite})`}}>
+      <div className="slider w-64 h-40 relative overflow-hidden rounded-sm border-white border-y-4 border-x-4 p-5">
+        {images.map((event, index) => (
+          <img
+            key={index}
+            src={event.post}
+            alt={`Post${index + 1}`}
+            className={`absolute top-0 left-0 w-full h-full transition-opacity duration-500 ${
+              index === slideIndex ? 'opacity-100' : 'opacity-0'
+            }`}
+          />
+        ))}
+      </div>
+      <div>
+      {images.map((event, index) => (
+            <h1>
+              {event.name}
+            </h1>
+        ))}
+      </div>
+      <div className="flex mt-4">
+        <button className="ml-4 px-4 py-2 text-black bg-blue-100 border-blue-500 border-b-4 hover:border-b-2 hover:border-t-2 border-blue-dark hover:border-blue rounded " onClick={prevSlide}>
+          <div class="w-0 h-0 
+            border-t-[0.4rem] border-t-transparent
+            border-r-[0.6rem] border-r-blue-500
+            border-b-[0.4rem] border-b-transparent">
+          </div>
+        </button>
+        <button className="ml-4 px-4 py-2 text-black bg-blue-100 border-blue-500 border-b-4 hover:border-b-2 hover:border-t-2 border-blue-dark hover:border-blue rounded " onClick={nextSlide}>
+          <div class="w-0 h-0 
+            border-t-[0.4rem] border-t-transparent
+            border-l-[0.6rem] border-l-blue-500
+            border-b-[0.4rem] border-b-transparent">
+          </div>
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default ImageSlider;
---
+++ src/images.js
@@ -0,0 +1,20 @@
+const image=[
+    {
+        name:"Hexbugs",
+        post:require('./images/image1.jpeg'),
+        id:0
+    },
+    {
+        name:"DevOps",
+        post:require('./images/image2.jpeg'),
+        id:1
+    },
+    {
+        name:"Cryptox",
+        post:require('./images/image3.jpeg'),
+        id:2
+    }
+];
+
+
+export default image

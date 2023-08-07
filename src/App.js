131027dd2e1d9da16777bdc92a1f1fa55ed3d66a
alirashidAR <110668489+alirashidAR@users.noreply.github.com>
2023-08-07 20:40:24 +0530

    new background added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState,useEffect} from 'react';
 import './index.css';
 import images from './images.js' /* This is to export the images object the image object which contains event pic and image descrption */
 import backgrounds from './backgrounds.js'
-import Planet from './planetsContainer.js';
+
 
   const App=()=>{
     return(
@@ -34,53 +34,56 @@ import Planet from './planetsContainer.js';
   };
 
 
-  return (
-    // <div className="flex flex-col items-center justify-center h-screen"style={{'backgroundImage':`url(${backgrounds.backgroundwebsite})`}}>
-    //   <p className="text-4xl max-md:text-sm text-white font-bold text-center my-8">ENGAGEMENTS</p>
-    //   <div className="slider w-80 max-md:w-60 h-60 max-md:h-40 relative overflow-hidden rounded-2xl  border-blue-500 border-y-4 border-x-4 p-5">
-    //     {images.map((event, index) => (
-    //       <img
-    //         key={index}
-    //         src={event.post}
-    //         alt={`Post${index + 1}`}
-    //         // Rendering of the image using opacity
-    //         className={`rounded-3xl absolute top-0 left-0 w-full h-full transition-opacity duration-500 p-4  ${
-    //           index === slideIndex ? 'opacity-100' : 'opacity-0'
-    //         }` }
-    //       />
-    //     ))}
-    //   </div>
-    //   <div>
-    //     {images.map((event, index) => (
-    //       // Conditional rendering to display event.name only when its image is active
-    //       index === slideIndex &&(
-    //         <h1 className='text-white max-md:text-sm' key={index}>
-    //           {event.name}
-    //         </h1>
-    //       )
-    //     ))}
-    //   </div>
-    //   <div className="flex mt-4">
-    //     <button className="ml-4 px-4 py-2 text-black bg-blue-100 border-blue-500 border-b-4 hover:border-b-2 hover:border-t-2 border-blue-dark hover:border-blue rounded " onClick={prevSlide}>
-    //       <div class="w-0 h-0 
-    //         border-t-[0.4rem] border-t-transparent
-    //         border-r-[0.6rem] border-r-blue-500
-    //         border-b-[0.4rem] border-b-transparent">
-    //       </div>
-    //     </button>
-    //     <button className="ml-4 px-4 py-2 text-black bg-blue-100 border-blue-500 border-b-4 hover:border-b-2 hover:border-t-2 border-blue-dark hover:border-blue rounded " onClick={nextSlide}>
-    //       <div class="w-0 h-0 
-    //         border-t-[0.4rem] border-t-transparent
-    //         border-l-[0.6rem] border-l-blue-500
-    //         border-b-[0.4rem] border-b-transparent">
-    //       </div>
-    //     </button>
-    //   </div>
-    // </div>
+  return (/*
+     <div className="flex flex-col items-center justify-center h-screen relative min-h-screen bg-cover bg-center "style={{'backgroundImage':`url(${backgrounds.backgroundwebsite})`}}>
+       <p className="heading text-4xl max-md:text-sm font-bold text-center my-8">ENGAGEMENTS</p>
+      <div className="slider w-72 h-56 max-md:w-48 max-md:h-44 relative overflow-hidden p-5">
+        {images.map((event, index) => (
+        <img
+             key={index}
+             src={event.post}
+             alt={`Post${index + 1}`}
+             // Rendering of the image using opacity
+             className={`rounded-3xl absolute top-0 left-0 w-full h-full transition-opacity duration-500 p-4  ${
+               index === slideIndex ? 'opacity-100' : 'opacity-0'
+             }` }
+           />
+         ))}
+       </div>
+       <div>
+       </div>
+            <div className="flex flex-wrap m-6 gap-8 max-md:gap-1">
+        <button className="ml-4 px-8 py-2 text-black bg-blue-100 border-blue-500 border-b-4 hover:border-b-2 hover:border-t-2 border-blue-dark hover:border-blue rounded max-md:px-4 max-md:py-1" onClick={prevSlide}>
+          <div class="w-0 h-0 
+            border-t-[0.4rem] border-t-transparent
+            border-r-[0.6rem] border-r-blue-500
+            border-b-[0.4rem] border-b-transparent">
+          </div>
+        </button>
+        <div>
+          {images.map((event, index) => (
+            // Conditional rendering to display event.name only when its image is active
+            index === slideIndex &&(
+              <h1 className='text-white max-md:text-sm pl-4' key={index}>
+                {event.name}
+              </h1>
+            )
+          ))}
+        </div>
+        <button className="ml-4 px-8 py-2 text-black bg-blue-100 border-blue-500 border-b-4 hover:border-b-2 hover:border-t-2 border-blue-dark hover:border-blue rounded max-md:px-4 " onClick={nextSlide}>
+          <div class="w-0 h-0 
+            border-t-[0.4rem] border-t-transparent
+            border-l-[0.6rem] border-l-blue-500
+            border-b-[0.4rem] border-b-transparent">
+          </div>
+        </button>
+      </div>
+    </div>
 
-    <div className="flex flex-col items-center justify-center h-screen"style={{'backgroundImage':`url(${backgrounds.backgroundwebsite})`}}>
+     */
+    <div className="flex flex-col items-center justify-center h-screen relative min-h-screen bg-cover bg-center "style={{'backgroundImage':`url(${backgrounds.backgroundwebsite})`}}>
       <p className="heading text-4xl max-md:text-sm font-bold text-center my-8">ENGAGEMENTS</p>
-      <div className="hexagon slider md:w-2/5 h-3/5 w-4/5 relative overflow-hidden border-blue-500 border-y-4 border-x-4 rounded-md p-5">
+      <div className="slider  w-72 h-56 max-md:w-48 max-md:h-44 relative overflow-hidden">
         {images.map((event, index) => (
           <img
             key={index}
@@ -95,7 +98,7 @@ import Planet from './planetsContainer.js';
       </div>
       
       <div className="flex flex-wrap m-6 gap-8">
-        <button className="ml-4 px-4 py-2 text-black bg-blue-100 border-blue-500 border-b-4 hover:border-b-2 hover:border-t-2 border-blue-dark hover:border-blue rounded " onClick={prevSlide}>
+        <button className="ml-4 px-8 py-2 text-black bg-blue-100 border-blue-500 border-b-4 hover:border-b-2 hover:border-t-2 border-blue-dark hover:border-blue rounded " onClick={prevSlide}>
           <div class="w-0 h-0 
             border-t-[0.4rem] border-t-transparent
             border-r-[0.6rem] border-r-blue-500
@@ -112,7 +115,7 @@ import Planet from './planetsContainer.js';
             )
           ))}
         </div>
-        <button className="ml-4 px-4 py-2 text-black bg-blue-100 border-blue-500 border-b-4 hover:border-b-2 hover:border-t-2 border-blue-dark hover:border-blue rounded " onClick={nextSlide}>
+        <button className="ml-4 px-8 py-2 text-black bg-blue-100 border-blue-500 border-b-4 hover:border-b-2 hover:border-t-2 border-blue-dark hover:border-blue rounded " onClick={nextSlide}>
           <div class="w-0 h-0 
             border-t-[0.4rem] border-t-transparent
             border-l-[0.6rem] border-l-blue-500
@@ -121,8 +124,6 @@ import Planet from './planetsContainer.js';
         </button>
       </div>
     </div>
-
-    
   );
 };
 
